da18360afe3ab55d4286c80bed9da0ee70dbcb30
Martial Wehrling <wehrling@free.fr>
2022-12-03 15:00:12 +0300

    ✨ F04Bis : ORDER > Create a component LeftSide of NavBar

--- src/components/pages/order/LeftSide.js
+++ src/components/pages/order/LeftSide.js
@@ -1,5 +1,18 @@
 import React from "react";
+import Logo from "./../../reusable-ui/Logo";
+import styled from "styled-components";
 
-export default function LeftSide() {
-  return <div>Menu Left</div>;
+export default function RightSide() {
+  return (
+    <ContainerRightSideStyled>
+      <Logo />
+    </ContainerRightSideStyled>
+  );
 }
+
+const ContainerRightSideStyled = styled.div`
+  display: flex-inline;
+  border: 6px solid purple;
+  margin-left: 20px;
+  transform: s;
+`;
