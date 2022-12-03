3217e5f153b245943934cdb620575988bf151b4e
Martial Wehrling <wehrling@free.fr>
2022-12-03 14:05:05 +0300

    ✨ F04Bis : ORDER>NavBar> insert components RightSide and LeftSide

--- src/components/pages/order/Navbar.js
+++ src/components/pages/order/Navbar.js
@@ -1,15 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import RightSide from "./RightSide";
+import LeftSide from "./LeftSide";
 
 export default function Navbar() {
   return (
     <NavbarStyled>
-      <p>1</p>
-      <p>2</p>
+      <RightSide />
+      <LeftSide />
     </NavbarStyled>
   );
 }
 
 const NavbarStyled = styled.div`
   border: 2px solid blue;
+  margin-top: 20px;
 `;
