6dd83a84240fac74b9d08150391b660adab97372
Martial Wehrling <wehrling@free.fr>
2022-12-03 18:51:19 +0300

    💄 F04Bis : NAVBAR > LeftSide> add styling margin + dimensions

--- src/components/pages/order/LeftSide.js
+++ src/components/pages/order/LeftSide.js
@@ -1,18 +1,25 @@
 import React from "react";
 import Logo from "./../../reusable-ui/Logo";
 import styled from "styled-components";
+import { theme } from "../../../theme/index";
 
 export default function RightSide() {
+  //state
+
+  //comportement
+
+  //display
   return (
-    <ContainerRightSideStyled>
+    <ContainerLogoNavbarStyled>
       <Logo />
-    </ContainerRightSideStyled>
+    </ContainerLogoNavbarStyled>
   );
 }
 
-const ContainerRightSideStyled = styled.div`
-  display: flex-inline;
-  border: 6px solid purple;
+const ContainerLogoNavbarStyled = styled.div`
+  /* transform: scale(0.3); */
+  width: 240.48px;
+  height: 98.19px;
   margin-left: 20px;
-  transform: s;
+  border: 2px solid black;
 `;
