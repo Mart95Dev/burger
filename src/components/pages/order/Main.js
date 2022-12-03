817b94f74ef4c62195b380effce0425e1ae832a6
Martial Wehrling <wehrling@free.fr>
2022-12-03 13:50:19 +0300

    ✨ F04BIS: ORDER> Create component Main

---
+++ src/components/pages/order/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Main() {
+  return <MainStyled>Main</MainStyled>;
+}
+
+const MainStyled = styled.div`
+  border: 2px solid green;
+`;
