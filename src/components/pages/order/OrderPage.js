88c1d8cd9e5fec2e4d2b5b55af25088b68f24947
Martial Wehrling <wehrling@free.fr>
2022-12-04 12:00:18 +0300

    💄 F04Bis: ORDER > change styling responsive

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -19,13 +19,15 @@ const BackgroundOrderPageStyled = styled.div`
   background-color: #ff9f1a;
   height: 100vh;
   overflow: hidden;
+  z-index: 999;
 `;
 
 const ContainerStyled = styled.div`
   width: 98%;
+  height: 100vh;
   margin-left: auto;
   margin-right: auto;
-
+  margin-bottom: 0px;
   @media screen and (min-width: 1520px) {
     max-width: 1400px;
   }
