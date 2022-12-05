dfacba1aaedffb1270910549f050ec6ace11dc96
Martial Wehrling <wehrling@free.fr>
2022-12-05 19:31:40 +0300

    🚧 F04Bis > ORDER >MAIN > Add  and update styling styled component

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -7,8 +7,10 @@ export default function Main() {
 
 const MainStyled = styled.div`
   text-align: center;
-  background: #f5f5f7;
+  background: green;
+  flex: 1;
+  /* background: #f5f5f7;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   border-radius: 0px 0px 15px 15px;
-  height: 81vh;
+  */
 `;
