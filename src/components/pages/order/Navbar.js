ec857013452d93719cc07201599b49788d8fee66
Martial Wehrling <wehrling@free.fr>
2022-12-05 19:48:11 +0300

    🚧 F04Bis : ORDER > Layout > navbar > add left-side and right-side

--- src/components/pages/order/Navbar.js
+++ src/components/pages/order/Navbar.js
@@ -1,13 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-import RightSide from "./RightSide";
-import LeftSide from "./LeftSide";
+import { Link, useParams } from "react-router-dom";
+// import RightSide from "./RightSide";
+// import LeftSide from "./LeftSide";
 
 export default function Navbar() {
+  //
+  const { username } = useParams();
+  //
   return (
     <NavbarStyled>
-      <LeftSide />
-      <RightSide />
+      <div className="left-side">Left</div>
+      <div className="right-side">
+        <h1>
+          Hey, <span className="username">{username}</span>
+        </h1>
+        <Link to="/">
+          <button>Se déconnecter</button>
+        </Link>
+      </div>
+
+      {/* <LeftSide />
+      <RightSide /> */}
     </NavbarStyled>
   );
 }
@@ -19,6 +33,13 @@ const NavbarStyled = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border-radius: 15px 15px 0 0;
-  font-family: "Open Sans", sans-serif;
+  /* border-radius: 15px 15px 0 0;
+  font-family: "Open Sans", sans-serif; */
+
+  .left-side {
+    background: pink;
+  }
+  .right-side {
+    background: purple;
+  }
 `;
