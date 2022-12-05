6a9ef6f66c3a7c0a759c6a6ef6542df2da8bcb11
Martial Wehrling <wehrling@free.fr>
2022-12-05 21:12:33 +0300

    💄 F04 : ORDER > Navbar > refacto styling

--- src/components/pages/order/Navbar.js
+++ src/components/pages/order/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import RightSide from "./RightSide";
 import LeftSide from "./LeftSide";
+import { theme } from "./../../../theme/index";
 
 export default function Navbar() {
   return (
@@ -13,13 +14,12 @@ export default function Navbar() {
 }
 
 const NavbarStyled = styled.nav`
-  /* background-color: white; */
-  background: blue;
+  background-color: ${theme.colors.white};
   height: 10vh;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-left: 20px;
-  /* border-radius: 15px 15px 0 0;
-   */
+  padding-left: ${theme.spacing.md};
+  border-radius: ${theme.borderRadius.extraRound}
+    ${theme.borderRadius.extraRound} 0 0;
 `;
