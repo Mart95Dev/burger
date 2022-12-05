c3a163da4780107032e1068b66fdd0e4c1c663e5
Martial Wehrling <wehrling@free.fr>
2022-12-05 18:44:52 +0300

    🚧 F04 : ORDER > layout >Add container

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,34 +1,31 @@
 import React from "react";
-
 import styled from "styled-components";
-import Navbar from "./Navbar";
-import Main from "./Main";
+// import Navbar from "./Navbar";
+// import Main from "./Main";
 
 export default function OrderPage() {
   return (
-    <BackgroundOrderPageStyled>
-      <ContainerStyled>
-        <Navbar />
-        <Main />
-      </ContainerStyled>
-    </BackgroundOrderPageStyled>
+    <OrderPageStyled>
+      <div className="container">
+        <p>kscqsncnqskcnqscnqsk</p>
+        <p>nckncqknck</p>
+        {/* <Navbar />
+        <Main /> */}
+      </div>
+    </OrderPageStyled>
   );
 }
 
-const BackgroundOrderPageStyled = styled.div`
-  background-color: #ff9f1a;
+const OrderPageStyled = styled.div`
+  background-color: orange;
   height: 100vh;
-  overflow: hidden;
-  z-index: 999;
-`;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-const ContainerStyled = styled.div`
-  width: 95%;
-  height: 100vh;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 0px;
-  @media screen and (min-width: 1520px) {
-    max-width: 1400px;
+  .container {
+    background: red;
+    height: 95vh;
+    width: 1400px;
   }
 `;
