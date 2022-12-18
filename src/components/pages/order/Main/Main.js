e462298e801e0a258d9577d3379dd0662ee3d066
Martial Wehrling <wehrling@free.fr>
2022-12-18 16:20:02 +0300

    ✨ F07 : ORDER > MAIN > Add components <Basket/> and <PanelAdmin/>

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -2,11 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import { theme } from "../../../../theme/index";
 import Menu from "../Main/Menu";
+import Basket from "./Basket";
+import PanelAdmin from "./Panel/PanelAdmin";
 
 export default function Main() {
   return (
     <MainStyled>
+      <Basket />
       <Menu />
+      <PanelAdmin />
     </MainStyled>
   );
 }
@@ -16,10 +20,8 @@ const MainStyled = styled.div`
   background: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   border-radius: 0px 0px 15px 15px;
-
   display: grid;
-  /* grid-template-columns: 25% 1fr; */
-  grid-template-columns: 1fr;
+  grid-template-columns: 25% 1fr;
 
   overflow-y: scroll;
 `;
