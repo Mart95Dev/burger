9a180636611059605edfefc14e316a21fe538dcd
Martial Wehrling <wehrling@free.fr>
2022-12-31 19:19:26 +0300

    🚧 F07Bis : ORDER> MAIN > update code to comment basket component

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import { theme } from "../../../../theme/index";
 import Menu from "../Main/Menu";
-import Basket from "./Basket";
+// import Basket from "./Basket";
 import ContainerTabsAdmin from "../Main/Panel/ContainerTabsAdmin";
 
 export default function Main() {
   return (
     <MainStyled>
-      <Basket />
+      {/* <Basket /> désactiver pour la mise en place du panel*/}
       <Menu />
       <ContainerTabsAdmin />
     </MainStyled>
@@ -21,7 +21,8 @@ const MainStyled = styled.div`
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   border-radius: 0px 0px 15px 15px;
   display: grid;
-  grid-template-columns: 25% 1fr;
+  /* grid-template-columns: 25% 1fr; désactiver pour la mise en place du panel */
+  grid-template-columns: 1fr;
   /* overflow-x: hidden; */
   overflow: hidden;
 `;
