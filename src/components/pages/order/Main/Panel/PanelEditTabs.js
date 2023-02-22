f0488d196d5f067769f2d271cef91f65d68e877c
Martial Wehrling <wehrling@free.fr>
2023-02-22 13:39:26 +0300

    🚧 F08 : PANEL > add text on tab edit and icon click + animation css

--- src/components/pages/order/Main/Panel/PanelEditTabs.js
+++ src/components/pages/order/Main/Panel/PanelEditTabs.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 import { useContext } from "react";
 import PanelContext from "../../../../context/OrderContext";
 import FormAdminPanel from "./../../../../reusable-ui/FormAdminPanel";
+
 import { theme } from "./../../../../../theme/index";
+import { HiCursorClick } from "react-icons/hi";
 
 function PanelEditTabs() {
   //state
@@ -14,7 +16,16 @@ function PanelEditTabs() {
     <>
       <PanelEditStyled>
         <div>{tabSelected === "add" ? <FormAdminPanel /> : ""}</div>
-        <div>{tabSelected === "edit" ? <FormAdminPanel /> : ""}</div>
+        <div className="edit-text">
+          {tabSelected === "edit" ? (
+            <span>
+              Cliquer sur un produit du menu pour le modifier span{" "}
+              <HiCursorClick className="icon" />
+            </span>
+          ) : (
+            ""
+          )}
+        </div>
       </PanelEditStyled>
     </>
   );
@@ -32,6 +43,34 @@ const PanelEditStyled = styled.div`
   height: 250px;
   padding-top: 17px;
   padding-left: 21px;
+
+  .edit-text {
+    height: 100%;
+    width: 50%;
+    position: relative;
+    top: 50px;
+
+    span {
+      color: rgb(116, 123, 145);
+      font-family: "Amatic SC", cursive;
+      font-size: 24px;
+      animation: fadeIn 1000ms ease 0s 1 normal;
+
+      .icon {
+        margin-left: 10px;
+      }
+    }
+
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+      }
+
+      to {
+        opacity: 1;
+      }
+    }
+  }
 
   /* display: flex;
   justify-content: start; */
