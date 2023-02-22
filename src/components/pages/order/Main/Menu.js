2b157a48a9d1f7614450c53d671dffa607a7eca3
Martial Wehrling <wehrling@free.fr>
2023-02-22 15:40:46 +0300

    💄 F08: ORDER > update style and props <Card/> and <Menu/>

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -15,6 +15,7 @@ function Menu() {
     <MenuStyled>
       {products.map(({ id, title, imageSource, price }) => (
         <Card
+          className="card-mode-admin-panel"
           key={id}
           title={title}
           imageSource={imageSource}
@@ -30,9 +31,25 @@ export default Menu;
 const MenuStyled = styled.div`
   background: $(theme.colors.background_white);
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  /* grid-template-columns: repeat(4, 1fr); */
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
   overflow-y: scroll;
+
+  .card-mode-admin-panel:hover {
+    transform: scale(1.05);
+    box-shadow: rgb(255 154 35) 0px 0px 8px 0px;
+    animation: scale 500ms ease-in-out 0s 1 normal;
+  }
+
+  @keyframes scale {
+    from {
+      transform: scale(1);
+    }
+    to {
+      transform: scale(1.05);
+    }
+  }
 `;
