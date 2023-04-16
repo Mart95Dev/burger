e134a081cdb727679062fcacf14d268f6ae6e833
Martial Wehrling <wehrling@free.fr>
2023-04-16 16:03:46 +0200

    💄 F08bis: ADMIN>Panel>Form> Add styling ans update code

--- src/components/pages/order/Main/Panel/menu/EmptyMenuAdmin.js
+++ src/components/pages/order/Main/Panel/menu/EmptyMenuAdmin.js
@@ -1,17 +1,48 @@
-import React from "react";
 import styled from "styled-components";
+import { theme } from "../../../../../../theme";
+import Button from "../../../../../reusable-ui/Button";
 
-function EmptyMenuAdmin({ onReset }) {
+export default function EmptyMenuAdmin({ onReset }) {
   return (
-    <EmptyMenuAdminStyled>
-      <div>
-        <span>Pas de produit</span>
-        <button onClick={onReset}>Remettre des produits</button>
-      </div>
-    </EmptyMenuAdminStyled>
+    <EmptyMenuStyled>
+      <span className="title">Le menu est vide ?</span>
+      <span className="description">
+        Cliquez ci-dessous pour le réinitialiser
+      </span>
+      <Button label={"Générer de nouveaux produits"} onClick={onReset} />
+    </EmptyMenuStyled>
   );
 }
 
-export default EmptyMenuAdmin;
+const EmptyMenuStyled = styled.div`
+  background-color: ${theme.colors.background_white};
+  box-shadow: ${theme.shadows.strong};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center; // permet de diminuer la largeur du bouton resetMenu
 
-const EmptyMenuAdminStyled = styled.div``;
+  .title,
+  .description {
+    text-align: center;
+    font-family: "Amatic SC", cursive;
+    color: ${theme.colors.greyBlue};
+  }
+
+  .title {
+    font-size: ${theme.fonts.size.P4};
+    font-weight: ${theme.fonts.weights.semiBold};
+  }
+
+  .description {
+    font-size: ${theme.fonts.size.P4};
+    margin-top: 20px;
+  }
+
+  button {
+    margin-top: 30px;
+    font-size: ${theme.fonts.size.XS};
+    width: auto;
+  }
+`;
