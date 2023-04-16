5576127b2b2cd9d3e1c2c84d758be3906e613991
Martial Wehrling <wehrling@free.fr>
2023-04-16 16:00:50 +0200

    ♻️ F08bis: ADMIN>Panel > Form > Add components and update style

--- src/components/pages/order/Main/Panel/FormAdmin/AddForm.js
+++ src/components/pages/order/Main/Panel/FormAdmin/AddForm.js
@@ -1,11 +1,16 @@
 import { useState, useContext } from "react";
 import styled from "styled-components";
 import PanelContext from "../../../../../context/OrderContext";
-import { FiCheck } from "react-icons/fi";
+// import { FiCheck } from "react-icons/fi";
 // import { FaHamburger } from "react-icons/fa";
 // import { BsFillCameraFill } from "react-icons/bs";
 // import { MdOutlineEuro } from "react-icons/md";
 import { theme } from "./../../../../../../theme/index";
+import Button from "./../../../../../reusable-ui/Button";
+import SubmitMessage from "./SubmitMessage";
+import ImagePreview from "./ImagePreview";
+import { getInputTextsConfig } from "./inputTextConfig";
+import TextInput from "./../../../../../reusable-ui/TextInput";
 
 const EMPTY_PRODUCT = {
   id: "",
@@ -33,6 +38,8 @@ export default function AddForm() {
     }, 2000);
   };
 
+  const inputTexts = getInputTextsConfig(newProduct);
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -49,52 +56,33 @@ export default function AddForm() {
 
   return (
     <AddFormStyled onSubmit={handleSubmit}>
-      <div className="image-preview">
-        {newProduct.imageSource ? (
-          <img src={newProduct.imageSource} alt={newProduct.title} />
-        ) : (
-          <div className="empty-image">Aucune image</div>
-        )}
-      </div>
-      <div className="inputs-fields">
-        <input
-          name="title"
-          value={newProduct.title}
-          type="text"
-          placeholder="Nom du produit (ex: Super Burger)"
-          onChange={handleChange}
-        />
-        <input
-          name="imageSource"
-          value={newProduct.imageSource}
-          type="text"
-          placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
-          onChange={handleChange}
-        />
-        <input
-          name="price"
-          value={newProduct.price ? newProduct.price : ""}
-          type="number"
-          placeholder="Prix"
-          onChange={handleChange}
-        />
+      <ImagePreview
+        imageSource={newProduct.imageSource}
+        title={newProduct.title}
+      />
+      <div className="input-fields">
+        {inputTexts.map((input) => (
+          <TextInput
+            {...input}
+            key={input.id}
+            onChange={handleChange}
+            version="minimalist"
+          />
+        ))}
       </div>
       <div className="submit">
-        <button className="submit-button">Ajouter un produit</button>
-        {isSubmitted ? (
-          <span className="submit-message">
-            <FiCheck /> Ajouté avec succès !
-          </span>
-        ) : (
-          ""
-        )}
+        <Button
+          className="submit-button"
+          label={"Ajouter un nouveau produit au menu"}
+          version="success"
+        />
+        {isSubmitted && <SubmitMessage />}
       </div>
     </AddFormStyled>
   );
 }
 
 const AddFormStyled = styled.form`
-  /* border: 2px solid red; */
   display: grid;
   grid-template-columns: 1fr 3fr;
   grid-template-rows: repeat(4, 1fr);
@@ -104,7 +92,6 @@ const AddFormStyled = styled.form`
   grid-row-gap: 8px;
 
   .image-preview {
-    /* background: blue; */
     grid-area: 1/1/4/2;
 
     img {
@@ -127,15 +114,14 @@ const AddFormStyled = styled.form`
     }
   }
 
-  .inputs-fields {
-    background: orange;
-    grid-area: 1/2/4/3;
+  .input-fields {
+    grid-area: 1/2/-2/3;
 
     display: grid;
+    grid-row-gap: 8px;
   }
 
   .submit {
-    background: green;
     grid-area: 4/2/5/3;
     display: flex;
     align-items: center;
