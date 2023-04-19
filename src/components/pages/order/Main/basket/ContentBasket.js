753e1a31ccbb67e7e20c2b57a4bfb09f13e02859
Martial Wehrling <wehrling@free.fr>
2023-04-19 12:05:05 +0200

    ✨ F10: MAIN>Basket> Create <ContentBasket/>

---
+++ src/components/pages/order/Main/basket/ContentBasket.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+function ContentBasket({ className }) {
+  return (
+    <ContentBasketStyled className={className}>
+      <span>Votre commande est vide</span>
+    </ContentBasketStyled>
+  );
+}
+
+export default ContentBasket;
+
+const ContentBasketStyled = styled.div`
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  justify-content: center;
+  overflow-y: scroll;
+
+  span {
+    font-family: "Amatic SC";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 72px;
+
+    color: #747b91;
+  }
+`;
