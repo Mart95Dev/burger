0d6b03a904ca038cfe2deb1899cb0ea95383f7a9
Martial Wehrling <wehrling@free.fr>
2023-04-19 11:51:25 +0200

    ✨ F10: MAIN > Basket> create <HerderBasket/>

--- src/components/pages/order/Main/basket/HeaderBasket.js
+++ src/components/pages/order/Main/basket/HeaderBasket.js
@@ -4,12 +4,40 @@ import styled from "styled-components";
 function HeaderBasket() {
   return (
     <HeaderBasketStyled>
-      <span>total</span>
-      <span>0,00€</span>
+      <span className="total">total</span>
+      <span className="addition">0,00€</span>
     </HeaderBasketStyled>
   );
 }
 
 export default HeaderBasket;
 
-const HeaderBasketStyled = styled.div``;
+const HeaderBasketStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 16px;
+  border: 1px solid red;
+  background: black;
+  color: #ffa01b;
+  height: 70px;
+  z-index: 3;
+
+  .total {
+    font-family: "Amatic SC";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 45px;
+  }
+
+  .addition {
+    font-family: "Amatic SC";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 36px;
+    line-height: 45px;
+    display: flex;
+    letter-spacing: 2px;
+  }
+`;
