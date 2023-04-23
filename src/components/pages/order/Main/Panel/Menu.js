4b62b6d26e907b159fcec5c3a3f9c30d53f90da7
Martial Wehrling <wehrling@free.fr>
2023-04-23 15:45:40 +0200

    🐛 F10bis: MENU> fix bug with props and clicked Card

--- src/components/pages/order/Main/Panel/Menu.js
+++ src/components/pages/order/Main/Panel/Menu.js
@@ -1,56 +1,86 @@
 import { useContext } from "react";
+import PanelContext from "../../../../../context/OrderContext";
 import styled from "styled-components";
-import PanelContext from "../../../../../../context/OrderContext";
-import { theme } from "../../../../../../theme";
-import { formatPrice } from "../../../../../../utils/maths";
 import Card from "../../../../../reusable-ui/Card";
+import { formatPrice } from "./../../../../../../utils/maths";
 import EmptyMenuAdmin from "./EmptyMenuAdmin";
 import EmptyMenuClient from "./EmptyMenuClient";
-import { checkIfProductIsClicked } from './menu/helper';
+import { checkIfProductIsClicked } from "./helper";
+import { EMPTY_PRODUCT } from "./../../../../../../enums/product";
 
-const IMAGE_BY_DEFAULT = "/images/coming-soon.png";
+const DEFAULT_IMAGE = "/images/coming-soon.png";
 
-export default function Menu() {
-  const { menu, isModeAdmin, handleDelete, resetMenu, productSelected } =
-    useContext(PanelContext);
-  // state
+function Menu() {
+  const {
+    menu,
+    isModeAdmin,
+    handleDelete,
+    resetMenu,
+    productSelected,
+    setproductSelected,
+    setTabSelected,
+    setIsCollasped,
+    // titleEditRef,
+    setHasAlreadyBeenClicked,
+  } = useContext(PanelContext);
+  //state
 
- 
-}
-  // affichage
+  //comportement
+  const handleClick = async (idProductSelected) => {
+    if (!isModeAdmin) return;
+    await setIsCollasped(false);
+    await setTabSelected("edit");
+    const productClicked = menu.find(
+      (product) => product.id === idProductSelected
+    );
+    // await titleEditRef.current.focus(); //@FIXME
+    setproductSelected(productClicked);
+    setHasAlreadyBeenClicked(true);
+  };
+
+  const handleCardOnDelete = (e, idProductDelete) => {
+    e.stopPropagation();
+    handleDelete(idProductDelete);
+    setHasAlreadyBeenClicked(false);
+    setproductSelected(EMPTY_PRODUCT);
+  };
+
+  //affichage
   if (menu.length === 0) {
     if (!isModeAdmin) return <EmptyMenuClient />;
     return <EmptyMenuAdmin onReset={resetMenu} />;
   }
 
   return (
-    <MenuStyled className="menu">
-      {menu.map(({ id, title, imageSource, price }) => {
-        return (
-          <Card
-            key={id}
-            title={title}
-            imageSource={imageSource ? imageSource : IMAGE_BY_DEFAULT}
-            leftDescription={formatPrice(price)}
-            hasDeleteButton={isModeAdmin}
-            onDelete={() => handleDelete(id)}
-            isHoverable={isModeAdmin}
-            isSelected={checkIfProductIsClicked(id, productSelected)}
-          />
-        );
-      })}
+    <MenuStyled>
+      {menu.map(({ id, title, imageSource, price }) => (
+        <Card
+          key={id}
+          title={title}
+          imageSource={imageSource ? imageSource : DEFAULT_IMAGE}
+          leftDescription={formatPrice(price)}
+          hasDeleteButton={isModeAdmin}
+          onDelete={(e) => handleCardOnDelete(e, id)}
+          onClick={() => handleClick(id)}
+          isHoverable={isModeAdmin}
+          isSelected={checkIfProductIsClicked(id, productSelected.id)}
+          onCardDelete={handleCardOnDelete}
+        />
+      ))}
     </MenuStyled>
   );
 }
 
+export default Menu;
+
 const MenuStyled = styled.div`
-  background: ${theme.colors.background_white};
+  background: $(theme.colors.background_white);
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  /* grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); */
+
+  /* grid-template-columns: repeat(4, 1fr); */
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   overflow-y: scroll;
 `;
