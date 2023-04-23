97a5256bffbe89cdc9f28d0a8e63e043a8008c38
Martial Wehrling <wehrling@free.fr>
2023-04-23 16:04:13 +0200

    🐛 F10bis: MENU > bug with display Card of Menu

--- src/components/pages/order/Main/Panel/menu/Menu.js
+++ src/components/pages/order/Main/Panel/menu/Menu.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import PanelContext from "../../../../../context/OrderContext";
 import styled from "styled-components";
+import { theme } from "./../../../../../../theme/index";
 import Card from "../../../../../reusable-ui/Card";
 import { formatPrice } from "./../../../../../../utils/maths";
 import EmptyMenuAdmin from "./EmptyMenuAdmin";
@@ -74,13 +75,13 @@ function Menu() {
 export default Menu;
 
 const MenuStyled = styled.div`
-  background: $(theme.colors.background_white);
+  background: ${theme.colors.background_white};
   display: grid;
-
-  grid-template-columns: repeat(4, 1fr);
-  /* grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); */
+  /* grid-template-columns: repeat(4, 1fr); */
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   overflow-y: scroll;
 `;
